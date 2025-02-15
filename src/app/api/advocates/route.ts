71b731161e3e05a7cbbd0da0ca4b7dad205bdcf7
Gregory Ryan <gregoryryan@Gregorys-Mac-mini.local>
2025-02-15 16:26:29 -0500

    feat: connect to docker container and use nodejs/pg api/db; add try catch and handle errors;

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,22 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
   // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+  const { searchParams } = req.nextUrl
+  const { page, pageSize } = Object.fromEntries(searchParams.entries());
 
-  const data = advocateData;
+  try {
+  const data = await db.
+    select()
+    .from(advocates)
+    .limit(Number(pageSize))
+    .offset((Number(page) - 1) * Number(pageSize));
 
   return Response.json({ data });
+  } catch (e) {
+   console.error(e);
+   return Response.json({ ok: false })
+  }
 }
--- src/app/api/seed/route.ts
+++ src/app/api/seed/route.ts
@@ -3,7 +3,12 @@ import { advocates } from "../../../db/schema";
 import { advocateData } from "../../../db/seed/advocates";
 
 export async function POST() {
-  const records = await db.insert(advocates).values(advocateData).returning();
+  try {
+    const records = await db.insert(advocates).values(advocateData).returning();
 
-  return Response.json({ advocates: records });
+    return Response.json({ advocates: records });
+  } catch (e) {
+    console.error(e);
+    return Response.json({ ok: false });
+  }
 }
